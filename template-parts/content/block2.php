c79893af0a45cd13968cda1db23931bff09ed3c7
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-06-08 14:56:41 -0500

    v2.0.9 city modern

--- template-parts/content/block2.php
+++ template-parts/content/block2.php
@@ -10,5 +10,14 @@ namespace WP_Rig\WP_Rig;
 ?>
 <section class="block2">
 	<h2>Under Construction</h2>
-	<h4>Check back soon!</h4>
+	<?php $sloganloop = new \WP_Query( array( 'post_type' => 'slogan', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+<?php while( $sloganloop->have_posts() ) : $sloganloop->the_post();
+			$slogan			= get_field('slogan');
+			?>
+	<h4>
+	<?php echo $slogan; ?>
+	</h4>
+	<?php endwhile; wp_reset_query(); ?>
+
 </section> <!-- end .block2 -->
